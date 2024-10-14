aba1ec430ebbdcb15605d5ae2862afe965c34547
Mayank-Kapadane <mayank.kapadane2005@gmail.com>
2024-10-14 18:17:23 +0530

    make search bar working

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@ import { API_KEY } from "./config.js";
 // create a structure of card
 const blogContainer = document.getElementById("blog-container");
 
+// Make a search bar working
+const searchField = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
+
 async function fetchRandomNews() {
     try {
         /*
@@ -14,9 +18,11 @@ async function fetchRandomNews() {
         // For fetching the data visit the homepage of website and there are some examples. of data in json format.
         // The home page shows the URL by which we can fetch the data.
         // You can see the docs of the website to see the query string parameter
-        const apiUrl = "https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apikey=" + API_KEY;
-        
-        // Beacuse of the parameter pageSize=10 we're showing 10 blocks. 
+        const apiUrl =
+            "https://newsapi.org/v2/top-headlines?country=us&pageSize=20&apikey=" +
+            API_KEY;
+
+        // Beacuse of the parameter pageSize=10 we're showing 10 blocks.
         // That type parameter you can search on news api website.
 
         const response = await fetch(apiUrl);
@@ -25,7 +31,6 @@ async function fetchRandomNews() {
         console.log("Data =",data);
         **************************
         */
-       
 
         // we want to show the data in our card
         //  for show something we need to return something
@@ -36,12 +41,42 @@ async function fetchRandomNews() {
     }
 }
 
+searchButton.addEventListener("click", async () => {
+    const query = searchField.value.trim();
+    if (query === "") {
+        // Do nothig
+    } else {
+        try {
+            // again fetch data as per the query
+            const articles = await fetchNewsQuery(query);
+            displayBlogs(articles);
+        } catch (error) {
+            console.log("Error fetching news from input query", error);
+        }
+    }
+});
+
+async function fetchNewsQuery(query) {
+    try {
+        const apiUrl =
+            `https://newsapi.org/v2/everything?q=${query}&pageSize=20&apiKey=` +
+            API_KEY;
+
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        return data.articles;
+    } catch (error) {
+        console.log("Error ferching news by query ", error);
+        return [];
+    }
+}
+
 function displayBlogs(articles) {
     // This function will generate cards
     // also we need to one more thing if the website is showing some previously fetched data then first of all we want to remove that and after removing we'll simply replace it with newest one.
     blogContainer.innerHTML = "";
     articles.forEach((article) => {
-
         // We're going to create a same thing like this dyanmically using javascript
         /*
         <div class="blog-card">
@@ -50,14 +85,14 @@ function displayBlogs(articles) {
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, earum. Tempora, explicabo quidem cum sint ipsa harum fugit, dicta amet incidunt, quam quia. Lorem ipsum, dolor sit amet consectetur adipisicing elit. </p>
         </div>
         */
-        
+
         // create a div-tag
         const blogCard = document.createElement("div");
         blogCard.classList.add("blog-card");
-        
+
         // store the image somewhere
         const img = document.createElement("img");
-        
+
         // img-tag also have two attributes
         img.src = article.urlToImage;
         img.alt = article.title;
@@ -66,38 +101,43 @@ function displayBlogs(articles) {
         const title = document.createElement("h2");
         title.textContent = article.title;
 
-            // some titles are too large so add ... after some np. of word in title.
-            const truncatedTitle = article.title.length > 30 ? article.title.slice(0,30) + "..." : article.title; 
-            title.textContent = truncatedTitle;
+        // some titles are too large so add ... after some np. of word in title.
+        const truncatedTitle =
+            article.title.length > 30
+                ? article.title.slice(0, 30) + "..."
+                : article.title;
+        title.textContent = truncatedTitle;
 
         // create discription (p-tag)
         const description = document.createElement("p");
         description.textContent = article.description;
-        
-        // same way with discription.
-            if(!article.description){
-                description.textContent = "Click here to see Description";
-            
-            }
-            else{
 
-                const truncatedDescription = article.description.length > 120 ? article.description.slice(0,120) + "..." : article.description;
-                description.textContent = truncatedDescription;
-            }
+        // same way with discription.
+        if (!article.description) {
+            description.textContent = "Click here to see Description";
+        } else {
+            const truncatedDescription =
+                article.description.length > 120
+                    ? article.description.slice(0, 120) + "..."
+                    : article.description;
+            description.textContent = truncatedDescription;
+        }
 
         /* We have successfully created our components 
         Now, after creating our components we need to append those componets inside our main#blog-container
         for doing that ..,
         */
-       blogCard.appendChild(img);
-       blogCard.appendChild(title);
-       blogCard.appendChild(description);
+        blogCard.appendChild(img);
+        blogCard.appendChild(title);
+        blogCard.appendChild(description);
 
-       blogContainer.appendChild(blogCard);
+        // If someone click on the card then we want to show the News in new window
+        blogCard.addEventListener("click", () => {
+            window.open(article.url, "_blank");
+        });
 
-
-
-    })
+        blogContainer.appendChild(blogCard);
+    });
 }
 
 (async () => {
@@ -111,7 +151,7 @@ function displayBlogs(articles) {
          console.log("Articles =",articles);
         ********************
         */
-       displayBlogs(articles);
+        displayBlogs(articles);
     } catch (error) {
         console.error("Error fetching while random news");
     }
